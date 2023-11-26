2ac045d2fca925c59c8138d0a51b5555982d5099
Zain <vwxyzzain@gmail.com>
2023-11-27 04:04:30 +0500

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" objectFit="cover" layout="fill" alt="" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>OH YES, WELL BAKED SLICE OF PIZZA.</h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <p className={styles.text}>
+            lorem ipsum #304.
+            <br /> Lahore
+            <br /> (+92) 123-4567
+          </p>
+          <p className={styles.text}>
+            123 johar town #235.
+            <br /> Lahore, 512
+            <br /> (+92) 123-4567
+          </p>
+          <p className={styles.text}>
+            456 Canal Road #104.
+            <br /> Lahore, 523
+            <br /> (+92) 123-4567
+          </p>
+          <p className={styles.text}>
+            98 Muslim Town #125.
+            <br /> Lahore, 523
+            <br /> (+92) 123-4567
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>WORKING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY UNTIL FRIDAY
+            <br /> 9:00 – 22:00
+          </p>
+          <p className={styles.text}>
+            SATURDAY - SUNDAY
+            <br /> 12:00 – 24:00
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
